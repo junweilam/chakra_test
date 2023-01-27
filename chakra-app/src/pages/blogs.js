843fb352998b192342cabccbf96fcc6b50117b46
junweilam <91267859+junweilam@users.noreply.github.com>
2023-01-28 00:14:17 +0800

    Added buttons example

--- chakra-app/src/pages/blogs.js
+++ chakra-app/src/pages/blogs.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import FAIcons, { FaFacebook } from "react-icons/fa"
+
 const Blogs = () => {
     return (
-        <h1>Welcome to the Blogs Page</h1>
+        <>
+            <h1>Welcome to the Blogs Page</h1>
+
+            {/* Creating a button */}
+            <Button colorScheme="blue">
+                Button
+            </Button>
+
+            {/* Button teal outline */}
+            <Button colorScheme="teal" variant="outline">
+                Button
+            </Button>
+
+            {/* Facebook button */}
+            <Button colorScheme="facebook" leftIcon={<FaFacebook/>}>
+                Facebook
+            </Button>
+        </>
     );
 };
 
